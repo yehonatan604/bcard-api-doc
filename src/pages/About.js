13adfba28968a2ce8ef7742c3bfe9be83f1ee33c
Your Name <you@example.com>
2023-10-08 13:53:51 +0300

    more fix

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,32 +1,34 @@
 import React from "react";
+import nextKey from "generate-my-key";
 import aboutList from "../data/lists/about.list";
 import RequestsTable from "../components/tables/RequestsTable";
 import ResponsesTable from "../components/tables/ResponsesTable";
 import ResponseDecodeTable from "../components/tables/ResponseDecodeTable";
 
 const About = () => {
-    return (
-        <React.Fragment>
-            {
-                aboutList.map((item) => {
-                    const table = (
-                        item.tableType === 1 ? <RequestsTable /> :
-                            item.tableType === 2 ? <ResponsesTable /> :
-                                <ResponseDecodeTable />
-                    )
-                    return (
-                        <div className="about flex-center w-100">
-                            <h1 className='title'>{item.title}</h1>
-                            <div className='flex-start'>
-                                <p>{item.text}</p>
-                                {table}
-                            </div>
-                        </div>
-                    )
-                })
-            }
-        </React.Fragment>
-    );
-}
+  return (
+    <React.Fragment>
+      {aboutList.map((item) => {
+        const table =
+          item.tableType === 1 ? (
+            <RequestsTable />
+          ) : item.tableType === 2 ? (
+            <ResponsesTable />
+          ) : (
+            <ResponseDecodeTable />
+          );
+        return (
+          <div className="about flex-center w-100" key={nextKey()}>
+            <h1 className="title">{item.title}</h1>
+            <div className="flex-start">
+              <p>{item.text}</p>
+              {table}
+            </div>
+          </div>
+        );
+      })}
+    </React.Fragment>
+  );
+};
 
 export default About;
