0b7ed9e14e43a7ca7f8763aba1a432749b2f00be
Yehonatan Moravia <97249220+yehonatan604@users.noreply.github.com>
2023-10-08 18:03:59 +0300

    info

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
                     <h1 className="title main">BCard API V1.0</h1>
                     <div className='flex-start'>
                         <p>
-                            BCard API v1 is a set of HTTP endpoints that adhere to RESTful design principles and CRUD actions with predictable URIs. It uses standard HTTP response codes, authentication, and verbs. The API has consistent and well-formed JSON requests and responses with cursor-based pagination to simplify list handling. Error messages are descriptive and easy to understand. All functions of the Vultr customer portal are accessible via the API, enabling you to script complex unattended scenarios with any tool fluent in HTTP.
+                            BCard API v1 is a set of HTTP endpoints that adhere to RESTful design principles and CRUD actions with predictable URIs. It uses standard HTTP response codes, authentication, and verbs. The API has consistent and well-formed JSON requests and responses with cursor-based pagination to simplify list handling. Error messages are descriptive and easy to understand.
                         </p>
                     </div>
                 </div>
