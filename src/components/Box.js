ad79361a03f3f7ec9b4291c89f5de72b239a37ff
Your Name <you@example.com>
2023-10-08 13:42:20 +0300

    some changes

--- src/components/Box.js
+++ src/components/Box.js
@@ -3,39 +3,42 @@ import Form from "./form/Form";
 import UserContext from "../store/UserContext";
 
 const Box = ({ currEndpoint }) => {
-    const [isBoxOpen, setIsBoxOpen] = useState(false);
-    const { user } = useContext(UserContext);
-    const endpoint = currEndpoint;
+  const [isBoxOpen, setIsBoxOpen] = useState(false);
+  const { user } = useContext(UserContext);
+  const endpoint = currEndpoint;
 
-    const boxToggle = (e) => {
-        e.target.innerText = isBoxOpen ? '+' : '-'
-        setIsBoxOpen(!isBoxOpen);
-    }
+  const boxToggle = (e) => {
+    e.target.innerText = isBoxOpen ? "+" : "-";
+    setIsBoxOpen(!isBoxOpen);
+  };
 
-    const isAdmin = user.authLevel === 'Admin';
+  const isAdmin = user.authLevel === "Admin";
 
-    let lock;
-    if (isAdmin || user.authLevel === endpoint.authRequire || endpoint.authRequire === 'None') {
-        lock = <p className="lock">&#128275;</p>;
-    }
-    else {
-        lock = <p className="lock">&#128274;</p>;
-    }
+  let lock;
+  if (
+    isAdmin ||
+    user.authLevel === endpoint.authRequire ||
+    endpoint.authRequire === "None"
+  ) {
+    lock = <p className="lock">&#128275;</p>;
+  } else {
+    lock = <p className="lock">&#128274;</p>;
+  }
 
-    return (
-        <div className="box flex-center">
-            <div className="box-title flex-space">
-                <div className="flex-row">
-                    <button className="btn" onClick={boxToggle}>+</button>
-                    <h3>{endpoint.name}</h3>
-                </div>
-                <div className="flex-row">
-                    {lock}
-                </div>
-            </div>
-            {isBoxOpen && <Form endpoint={endpoint} />}
+  return (
+    <div className="box flex-center">
+      <div className="box-title flex-space">
+        <div className="flex-row">
+          <button className="btn" onClick={boxToggle}>
+            +
+          </button>
+          <h3>{endpoint.name}</h3>
         </div>
-    );
-}
+        <div className="flex-row">{lock}</div>
+      </div>
+      {isBoxOpen && <Form endpoint={endpoint} />}
+    </div>
+  );
+};
 
 export default Box;
